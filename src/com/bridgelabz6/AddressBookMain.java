fb3861f8c7f396168ea01b85d7ba9a8e416487d3
niharika-1999 <niharika.rao@ymedialabs.com>
2021-08-30 18:12:52 +0530

    Added:WelcomeMessage

---
+++ src/com/bridgelabz6/AddressBookMain.java
@@ -0,0 +1,14 @@
+package com.bridgelabz6;
+import java.util.*;
+
+public class AddressBookMain {
+		public ArrayList<AddressBookMain>  contact=new ArrayList<>();
+		public Scanner sc=new Scanner(System.in);
+		public AddressBookMain()
+		{
+			System.out.println("Welcome to Address Book Program");//Welcome Message
+		}
+
+	}
+
+
